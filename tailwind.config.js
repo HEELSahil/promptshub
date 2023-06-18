99b49485cb9326d3a07a34d2f3928086a96dad40
Kotian, Sahil Udaya <skotian@iu.edu>
2023-06-19 02:06:56 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      'animation': {
+        'text':'text 4s ease infinite',
       },
+      'keyframes': {
+        'text': {
+            '0%, 100%': {
+               'background-size':'200% 200%',
+                'background-position': 'left center'
+            },
+            '50%': {
+               'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        },
+      },
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
     },
   },
   plugins: [],
